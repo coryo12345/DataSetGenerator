4ebb4f495b388990f57a0a490271368af4b36c02
coryo12345 <corydio12@gmail.com>
2022-03-30 22:55:56 -0400

    added search to table

--- src/components/MasterDataOutput.vue
+++ src/components/MasterDataOutput.vue
@@ -3,6 +3,7 @@ import { computed, ref } from "vue";
 import { useDataStore } from "../store";
 // need to rename import since we use a component called 'Column'
 import * as CT from "../types/Columns"; // ColumnTypes
+import { FilterMatchMode } from "primevue/api";
 
 const dataStore = useDataStore();
 
@@ -12,6 +13,7 @@ function exportCSV() {
 }
 
 const data = ref([] as any[]);
+const tableSearch = ref("");
 
 function generateData(newCols: CT.Column[], count: number): void {
   const newData = [];
@@ -29,6 +31,20 @@ const cols = computed(() => {
   generateData(dataStore.columns, dataStore.rowCount);
   return dataStore.columns;
 });
+
+const colFields = computed(() => {
+  return dataStore.columns.map((x) => x.internalId);
+});
+
+const fullFilters = computed(() => {
+  const f = {
+    global: { value: tableSearch.value, matchMode: FilterMatchMode.CONTAINS },
+  } as any;
+  colFields.value.forEach((colId) => {
+    f[colId] = { value: null, matchMode: FilterMatchMode.CONTAINS };
+  });
+  return f;
+});
 </script>
 
 <template>
@@ -42,23 +58,30 @@ const cols = computed(() => {
         :rows="10"
         paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
         :rowsPerPageOptions="[10, 20, 50]"
+        :globalFilterFields="colFields"
+        :filters="fullFilters"
         responsiveLayout="scroll"
         currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
         showGridlines
       >
         <template #header>
-          <div class="inline-block">
+          <div class="flex justify-content-between">
             <Button
               icon="pi pi-external-link"
               label="Export"
               @click="exportCSV()"
+              style="max-width: 10em"
             />
-            <!-- TODO: search -->
+            <span class="p-input-icon-left" style="max-width: 20em">
+              <i class="pi pi-search" />
+              <InputText v-model="tableSearch" placeholder="Keyword Search" />
+            </span>
           </div>
         </template>
         <Column
           v-for="col in cols"
           :field="col.internalId"
+          :filterField="col.internalId"
           :key="col.internalId"
           :header="col.displayName"
         ></Column>
